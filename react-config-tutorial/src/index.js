0ab82d22f59dbaf5786920b8afad8ade36f90da8
Begerad, Stefan <stefan@begerad.de>
2021-09-09 14:20:15 -0400

    chore: called script format and eslint-fix

--- react-config-tutorial/src/index.js
+++ react-config-tutorial/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./style/main.less";
+import './style/main.less';
 
 /*
  *component Welcome
@@ -8,8 +8,8 @@ import "./style/main.less";
  *Webpack needs Babel to process ES6 into ES5 syntaxes in order for this class to work
  */
 class Welcome extends React.Component {
-  render() {
-    return <h1>react-config-tutorial</h1>;
-  }
+    render () {
+        return <h1>react-config-tutorial</h1>;
+    }
 }
 ReactDOM.render(<Welcome />, document.getElementById('root'));
