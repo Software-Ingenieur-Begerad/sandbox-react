08f756262b87bf707635039a05efc07daa1fe366
Begerad, Stefan <stefan@begerad.de>
2021-09-09 13:15:28 -0400

    docu: added ducu

--- react-config-tutorial/src/index.js
+++ react-config-tutorial/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 /*
  *component Welcome
+ *using class syntax from ES6
+ *Webpack needs Babel to process ES6 into ES5 syntaxes in order for this class to work
  */
 class Welcome extends React.Component {
   render() {
