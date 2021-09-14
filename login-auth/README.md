5a37f2b54c5f5efe9a09a6e68b8b53298bf29d20
Begerad, Stefan <stefan@begerad.de>
2021-09-14 11:19:39 -0400

    chore: copied from branch config-tut in git@github.com:dancesWithCycles/react-sandbox.git

---
+++ login-auth/README.md
@@ -0,0 +1,1 @@
+[source](https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications)
---
+++ login-auth/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/main.less';
+
+/*
+ *component Welcome
+ *using class syntax from ES6
+ *Webpack needs Babel to process ES6 into ES5 syntaxes in order for this class to work
+ */
+class Welcome extends React.Component {
+    render () {
+        return <h1>react-config-tutorial</h1>;
+    }
+}
+ReactDOM.render(<Welcome />, document.getElementById('root'));
