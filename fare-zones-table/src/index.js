878416e2ba854ff757aa38952b75ec17c36dcaab
Begerad, Stefan <stefan@begerad.de>
2021-09-13 20:25:33 -0400

    fare-zones-table: initial commit

---
+++ fare-zones-table/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FareZones from './component/fare-zones';
+import './style/main.less';
+
+ReactDOM.render(<FareZones />, document.getElementById('root'));
