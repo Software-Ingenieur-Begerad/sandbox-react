2f495ba51c6b09773ea673327bf3c029ad1dcb0e
Begerad, Stefan <dialog@swingbe.de>
2022-07-18 18:20:15 +0200

    feat(auth0) add onClick prop to Button tag

--- auth0/app/components/login-button.jsx
+++ auth0/app/components/login-button.jsx
@@ -1,9 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
+
 function LoginButton(props) {
+    const { loginWithRedirect } = useAuth0()
     return (
         <div>
-            <button className="loginButton">Login</button>
+            <button 
+                onClick={()=>loginWithRedirect()} 
+                className="loginButton"
+                >
+                Login
+                </button>
         </div>
-    );
-};
+    )
+}
+
 export default LoginButton;
--- auth0/app/components/logout-button.jsx
+++ auth0/app/components/logout-button.jsx
@@ -1,9 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
+
 function LogoutButton(props) {
+    const { loginWithRedirect } = useAuth0()
     return (
         <div>
-            <button className="loginButton">Logout</button>
+            <button 
+                onClick={()=>loginWithRedirect()} 
+                className="loginButton"
+                >
+                Logout
+                </button>
         </div>
-    );
-};
+    )
+}
+
 export default LogoutButton;
