9e55024b00fb52527db276b30e64e7683fa405a0
Begerad, Stefan <dialog@swingbe.de>
2022-07-20 19:32:52 +0200

    feat(auth0) add Loading component

--- auth0-blog/app/app.jsx
+++ auth0-blog/app/app.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 import NavBar from "./components/nav-bar";
+import Loading from "./components/loading";
 import Home from './pages/home';
 export default function App() {
+    const { isLoading } = useAuth0();
+    if (isLoading) {
+	return <Loading />;
+    }
     return (
-	<div id="app" className="d-flex flex-column h-100">
+	<div>
 	    <NavBar />
 	    <div>
 		<h1>Auth0 with React.js</h1>
